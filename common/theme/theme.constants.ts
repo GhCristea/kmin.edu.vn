691848d419a61d1cdf5dbc3267fb16b3e6ec4443
Dustin Do <dustin.do95@gmail.com>
2021-03-05 12:09:50 +0700

    Add Proxima Nova font

--- common/theme/theme.constants.ts
+++ common/theme/theme.constants.ts
@@ -1,9 +1,10 @@
 export const BORDER_RADIUS = 4
-export const HEADING_FONT = `"OpenSans","Helvetica","Arial",sans-serif`
 export const PRIMARY_COLOR = '#103955'
 export const SECONDARY_COLOR = '#FFC82D'
-export const TEXT_FONT = `"OpenSans","Helvetica","Arial",sans-serif`
 export const ROUNDED_CORNER_RADIUS = 64
+
+export const TEXT_FONT = `"OpenSans","Helvetica","Arial",sans-serif`
+export const HEADING_FONT = `"Proxima Nova","OpenSans","Helvetica","Arial",sans-serif`
 
 export const LOGO_LIGHT_URL = '/assets/images/logo-light.svg'
 export const KMIN_FACEBOOK_URL = 'https://facebook.com/hoclaptrinhthatdedang'
